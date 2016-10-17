beae2b822bf6c7bbdcefd5881b300f6483c7f7d1
barrypearseburke <barry.burke3@mydit.ie>
2016-10-17 14:58:07 +0100

    more functionality, Awaiting ca clarification

--- customShell.py
+++ customShell.py
@@ -1,17 +1,19 @@
 import os
+import subprocess
 print "-----------------------------------------------\n\r"
-print "-------CUSTOM SHELL - BARRY BURKE -C13427078\n\r"
-print "------- GIVE US FULL MAKRS THERE -----------\n\r"
+print "-------CUSTOM SHELL - BARRY BURKE -C13427078---\n\r"
+print "------- GIVE US FULL MAKRS THERE --------------\n\r"
 
-allowedCmds ={"pw":"pwcmd","ifc":"ifccmd","ud":"udcmd","dt":"datecmd"}
+allowedCmds ={"help":"help","pw":"pwcmd","ifc":"ifccmd","ud":"udcmd","dt":"datecmd"}
 
-
+def help(options):
+    print("Available cmds {0}".format(key for key, value in allowedCmds.iteritems() ))
 def pwcmd(options):
         print "pwd"
 
 
 def datecmd(options):
-        print "in date cmd"
+        print "date +%Y%m%d%H%M%S"
 
 
 def ifccmd(options):
@@ -26,7 +28,12 @@ def udcmd(options):
         print "in udcmd"
 
 while (1==1):
-        UserInput = raw_input('>')
+        user = subprocess.check_output(['whoami'])
+        user=user.strip("\r\n")
+        hostname = subprocess.check_output(['hostname'])
+        hostname =hostname.strip("\r\n")
+        userprompt = ("{0}@{1}:>".format(user,hostname))
+        UserInput = raw_input(userprompt)
         options = UserInput.split(' ')
         for key, value in allowedCmds.iteritems():
                 if options[0] == key:
